5be3ace1026f064864560f2084d55ed7fbc9b955
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-28 18:34:57 -0400

    Update to fix output

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number
-if last_digit < 0:
-    last_digit *= -1
-last_digit = last_digit % 10
+if number < 0:
+    last_digit = number * -1
+    last_digit = number % 10
+else:
+    last_digit = number % 10
 if number > 5:
-    message = 'is greater than 5'
+    message = 'is greater than 5 and not 0'
 elif number < 0:
-    message = 'is less than 5'
+    message = 'is less than 5 and not 0'
 else:
     message = 'is 0'
 print("Last digit of %d is %d and %s" % (number,last_digit,message))
