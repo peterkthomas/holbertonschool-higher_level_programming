f81a82dab6fddd4b0484da81b9208d437a135f34
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-28 18:45:35 -0400

    pep8 fix

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -14,4 +14,4 @@ elif n == 0:
     message = 'is 0'
 else:
     message = 'is less than 6 and not 0'
-print("Last digit of %d is %d and %s" % (number,n,message))
+print("Last digit of %d is %d and %s" % (number, n, message))
