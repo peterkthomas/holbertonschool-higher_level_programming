e00bb741e83f70be11046d4199c2c22a3271e581
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-30 16:03:09 -0400

    task 13

---
+++ 0x02-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "list.h"
+
+/**
+ * check - recursive function to check a listint_t for a palindrome
+ * @head: head node of singly linked list
+ * @temp: will point to last item of list
+ *
+ * Return: 0 if not palindrome, 1 if it is
+ */
+int check(listint_t **head, listint_t *temp)
+{
+	if (!temp)
+		return (1);
+	if ((*head)->n == temp->n && check(head, temp->next))
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - calls check recursively for palindrome match
+ * @head: head node of list
+ *
+ * Return: 0 if not palindrome, 1 if it is
+ */
+int is_palindrome(listint_t **head)
+{
+	if (!head || !*head)
+		return (1);
+	return (check(head, *head));
+}
