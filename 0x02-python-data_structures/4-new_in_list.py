6c40a9ee62da943772f5295080e8dfdb622163d0
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-30 13:42:52 -0400

    Changed len to >=

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     list_copy = my_list[:]
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return my_list
     else:
         list_copy[idx] = element
