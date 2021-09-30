1fe5e8d1d4d9bf7c0399f2c0f239cf2f7fb61118
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-30 16:06:58 -0400

    fixed lists.h to version from project text

--- 0x02-python-data_structures/lists.h
+++ 0x02-python-data_structures/lists.h
@@ -1,8 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct listint_s - singly linked list
@@ -18,6 +15,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
 int is_palindrome(listint_t **head);
 
-#endif
+#endif /* LISTS_H */
