29fe09d78ffd98f8581143c45402a58c5a81445d
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-10-05 21:03:51 -0400

    adding body to roman integer converter

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    return
+    rn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    i = 0
+    if type(roman_string) == str:
+        while i < len(roman_string):
+            if i+1 < len(roman_string) and roman_string[i:i+2] in rn:
+                num += rn[roman_string[i:i+2]]
+                i += 2
+            else:
+                num += rn[roman_string[i]]
+                i += 1
+    return num
