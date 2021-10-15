b54de9be6d28870a7953a1af613895b3ee70cc2a
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-10-15 18:41:46 -0400

    task 2 commit

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    File: 2-append_write.py
+"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a utf-8 text file, appended"""
+    with open(filename, 'a', encoding='utf-8') as fd:
+        return fd.write(text)
