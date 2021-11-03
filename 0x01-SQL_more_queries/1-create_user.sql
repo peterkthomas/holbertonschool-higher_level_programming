808d9ab5eaab6b343918addabbac9b1287863e39
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-11-03 16:24:50 -0400

    Update 1-create_user.sql

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
 -- Creates MySQL user
 -- Adding second comment line!
-CREATE USER if not EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER if not EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost;
 FLUSH PRIVILEGES;
