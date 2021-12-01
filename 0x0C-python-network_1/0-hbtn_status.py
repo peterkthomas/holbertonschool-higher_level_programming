eae62f7d9bdf9cec58e4e66c74a2ac06c731564f
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-12-01 17:49:06 -0500

    pep8 tab fix

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -3,6 +3,6 @@
 import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as url:
-	s = url.read()
-	req = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-	print(req.format(type(s), s, s.decode('utf-8')))
+    s = url.read()
+    req = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+    print(req.format(type(s), s, s.decode('utf-8')))
