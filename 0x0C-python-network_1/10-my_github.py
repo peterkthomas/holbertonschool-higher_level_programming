779ca3ff1bda44403cd80e2857e3fa15c829fe56
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-12-01 20:00:12 -0500

    pep8 overindent fix

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     user = sys.argv[1]
     pw = sys.argv[2]
     r = requests.get('https://api.github.com/user',
-                       auth=(user, pw))
+                     auth=(user, pw))
     print(r.json().get('id'))
