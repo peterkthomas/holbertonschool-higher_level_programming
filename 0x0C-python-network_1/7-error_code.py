6ece64de7711df1193ee70d95407a3657534afae
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-12-01 19:17:52 -0500

    Create 7-error_code.py

---
+++ 0x0C-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""7-error_code.py"""
+
+import requests
+from requests.exceptions import HTTPError
+import sys
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
