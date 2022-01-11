a130ac6322bc02a7ffa55064e859371c93e41384
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2022-01-11 16:58:28 -0500

    fixed period that should have been a comma

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const fd = process.argv[2];
 const tw = process.argv[3];
 
-fs.writeFile(fd, tw, 'utf8'. function (err) {
+fs.writeFile(fd, tw, 'utf8', function (err) {
   if (err) {
     console.log(err);
   }
